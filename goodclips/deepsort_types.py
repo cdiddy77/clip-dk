33cf446f47294113037b2a7fb42c84cc3f13d05d
cdiddy77 <charles@parkerfolk.net>
2024-12-08 06:59:35 -0800

    script to download a clip

--- goodclips/deepsort_types.py
+++ goodclips/deepsort_types.py
@@ -1,3 +1,4 @@
+from typing import TypedDict
 from pydantic import BaseModel, Field
 
 
@@ -18,3 +19,12 @@ class DeepsortOutput(BaseModel):
         ..., description="List of frames with bounding boxes and identities"
     )
     object_id_names: list[str] = Field(..., description="List of object id names")
+
+
+class MovementByFrame(TypedDict):
+    ts: float
+    movement: float
+
+
+STANDARD_FPS = 30.0
+LABELSTUDIO_FPS = 24.0
